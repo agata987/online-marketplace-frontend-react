6fd241f7c88e9b0a780c77ee3fb06a76ee2529b2
agata <agata.szczypinska@gmail.com>
2020-10-29 16:44:49 +0100

    ---

---
+++ src/components/forms/CreateOfferForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button, Form} from 'semantic-ui-react'
+
+const CreateOfferForm = props => {
+
+    return(
+        <Form>
+            <h2>Tworzenie nowego ogłoszenia</h2>
+                   
+        </Form>
+    )
+    
+}
+
+export default CreateOfferForm
--- src/views/CreateOfferView.js
+++ src/views/CreateOfferView.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import CreateOfferForm from '../components/forms/CreateOfferForm'
 
 class CreateOfferView extends React.Component {
 
     render() {
         return (
             <div>
-                CreateOfferView
+                <CreateOfferForm />
             </div>
         );
     }
