55d006f62069e4497bbd76dd19cfe33589592456
agata <agata.szczypinska@gmail.com>
2020-11-02 21:18:09 +0100

    ---

--- src/views/CreateOfferView.js
+++ src/views/CreateOfferView.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import CreateOfferForm from '../components/forms/CreateOfferForm'
 import {connect} from 'react-redux'
+import {Loader} from 'semantic-ui-react'
 
 const CreateOfferView = props => {
     return (
         <div>
-            {props.user ? <CreateOfferForm user_id={props.user.id}/> :  null}
+            {props.user ? <CreateOfferForm user_id={props.user.id}/> :  <div style={{width: '100%', padding: '60px', display: 'flex', justifyContent: 'center'}}><Loader active inline /></div>}
         </div>
     );
 
