fd7aa7679b7a50a53c9543e27a291b02e6d548e2
agata <agata.szczypinska@gmail.com>
2020-11-09 01:09:32 +0100

    job offer view

--- src/components/JobOfferView.js
+++ src/components/JobOfferView.js
@@ -4,7 +4,8 @@ import {connect} from 'react-redux'
 import { 
     Button, 
     Modal,
-    Loader
+    Loader,
+    Icon
 } from 'semantic-ui-react'
 import API_Handler from '../API_Handler'
 import LoginInfoModal from './LoginInfoModal'
@@ -49,7 +50,45 @@ const JobOfferView = props => {
             >
                 {props.offer ? 
                 <Hoc>
-                    <h1> XD</h1>
+                    <Modal.Header>
+                        <span>{props.offer.name} <small>/ {props.offer.creation_date} </small>{userName !== '' ? <small> / {userName}</small> : null}   </span>       
+                    </Modal.Header>
+                    <Modal.Content>
+                        <Modal.Description>
+                            {props.city ? <h3>Miejsce pracy: {props.city.name}</h3> : null}
+                            {props.offer.company ? <h3>Firma: {props.offer.company}</h3> : null}
+                            {props.offer.remote ? <h3>Praca zdalna.</h3>: null}
+                            {props.offer.min_salary ? <h3>Zarobki: {props.offer.min_salary}{props.offer.max_salary ? <span> - {props.offer.max_salary}</span> : null} zł</h3> : null}
+                            
+                            {props.offer.description ? <hoc><h3>Opis:</h3> <h3>{props.offer.description}</h3></hoc> : null}
+                        </Modal.Description>
+                    </Modal.Content>
+
+                    <Modal.Actions>
+                        <div style={{marginBottom: '5px'}}>
+                            <span style={{marginLeft: '5px'}}>
+                                <Button onClick={()=>{}} color='red' animated>
+                                    <Button.Content hidden>Dodaj</Button.Content>
+                                    <Button.Content visible>
+                                        <Icon name='heart' />
+                                    </Button.Content>
+                                </Button>
+                            </span>
+                            <span style={{marginLeft: '5px'}}>
+                                <Button onClick={sendMessageHandle} color='blue'  animated>
+                                    <Button.Content visible>Wiadomość</Button.Content>
+                                    <Button.Content hidden>
+                                        <Icon name='arrow right' />
+                                    </Button.Content>
+                                </Button>
+                            </span>
+                            <span style={{marginLeft: '5px'}}>
+                                <Button onClick={props.onRequestClose}>
+                                    <Button.Content visible>Zamknij</Button.Content>
+                                </Button>
+                            </span>
+                        </div>
+                    </Modal.Actions>
                 </Hoc>
                 : <div style={{width: '100%', padding: '60px', display: 'flex', justifyContent: 'center'}}><Loader active inline /></div>}
             </Modal>
@@ -58,4 +97,11 @@ const JobOfferView = props => {
     )
 }
 
-export default JobOfferView
+const mapStateToProps = state => {
+    return {
+      user: state.authReducer.user,
+    }
+}
+
+
+export default connect(mapStateToProps)(JobOfferView)
